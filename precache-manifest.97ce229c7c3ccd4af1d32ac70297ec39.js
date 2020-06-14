beec528efb41b37c5226defc030d964af660458d
crestasom <crestasom@gmail.com>
2020-06-14 21:44:45 +0545

    Updates

---
+++ precache-manifest.97ce229c7c3ccd4af1d32ac70297ec39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d209bbedb9f09a48d7ea36b555cfeb5",
+    "url": "/react-client-panel/index.html"
+  },
+  {
+    "revision": "62ab5d29cea26c2165cb",
+    "url": "/react-client-panel/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c07de6ff44c650af0c8d",
+    "url": "/react-client-panel/static/js/2.67f571a1.chunk.js"
+  },
+  {
+    "revision": "61707c744984ea077ee9193ed349dd07",
+    "url": "/react-client-panel/static/js/2.67f571a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ab5d29cea26c2165cb",
+    "url": "/react-client-panel/static/js/main.36bea263.chunk.js"
+  },
+  {
+    "revision": "fb3b76a64fe3d345cdd1",
+    "url": "/react-client-panel/static/js/runtime-main.19f2e524.js"
+  }
+]);
